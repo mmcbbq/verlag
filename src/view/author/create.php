093aace10d020a9111714af40c3b15e85481e161
mmcbbq <m.martinez@bbq.de>
2025-05-20 15:50:45 +0200

    create html

--- src/view/author/create.php
+++ src/view/author/create.php
@@ -1,4 +1,25 @@
-<?php
+<!doctype html>
+<html lang='en'>
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport'
+          content='width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0'>
+    <meta http-equiv='X-UA-Compatible' content='ie=edge'>
+    <title>Document</title>
+</head>
+<body>
+<form action='author/new' method='post'>
+    fname: <input type='text' name='fname'>
+    lname: <input type='text' name='lname'>
+    bday: <input type='date' name='bday'>
+    country: <input type='text' name='country'>
+    <input type='submit'>
 
+</form>
+
+
+
+</body>
+</html>
 
 
