62dad74ab2a94c7a00182a299784e4a4557ae77b
mmcbbq <m.martinez@bbq.de>
2025-05-22 14:29:22 +0200

    aufgabe fertig

--- index.php
+++ index.php
@@ -17,19 +17,30 @@ spl_autoload_register(function ($className) {
 // Aufspalten der aktuellen URL in Segmente
 // Entfernt den Pfad "/verlag/" aus der URL und teilt den Rest anhand von "/"
 $url = explode('/', str_ireplace('/verlag/', '', $_SERVER['REQUEST_URI']));
-
+$url = array_diff($url, ['']);
 // Ermitteln des Controller-Namens aus dem ersten URL-Segment
-$controllerName = $url[0] . 'Controller';
-
+$controllerName = isset($url[0]) ? $url[0] . 'Controller' : 'HomeController';
+$controllerName = file_exists("src/Controller/$controllerName.php") ? $controllerName : '_404Controller';
 // Ermitteln der Methode aus dem zweiten URL-Segment
-$method = $url[1];
+$method = $url[1] ?? 'index';
 
-// Drittes URL-Segment als Parameter übergeben (z. B. eine ID), standardmäßig ein leerer Array
-$index = [(int)$url[2]] ?? [];
+// Drittes URL-Segment als Parameter übergeben (z.B. eine ID), standardmäßig ein leerer Array
+$index = isset($url[2]) ? [$url[2]] : [];
 
 // Erstellen einer Instanz des Controllers
 $controller = new $controllerName;
+if (!method_exists($controller, $method)) {
+    $controller = new _404Controller();
+    $method = 'index';
+} else {
+    $reflection = new ReflectionClass($controller);
+    $anzahlparams = $reflection->getMethod($method)->getNumberOfParameters();
+    if (($anzahlparams > 0 and !$index) or !is_numeric($index)){
+        $controller = new _404Controller();
+        $method = 'index';
+    }
 
+};
 // Aufruf der Methode des Controllers mit dem gegebenen Parameter
 call_user_func_array([$controller, $method], $index);
 
---
+++ src/Controller/_404Controller.php
@@ -0,0 +1,9 @@
+<?php
+class _404Controller
+{
+
+    public function index()
+    {
+        include "src/view/error/404.php";
+    }
+}
