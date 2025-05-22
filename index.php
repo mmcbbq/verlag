5c84bca8c81425fb1d15b631b5d1a54af60657e3
mmcbbq <m.martinez@bbq.de>
2025-05-22 11:08:59 +0200

    add comments

--- index.php
+++ index.php
@@ -1,40 +1,37 @@
 <?php
-// zum Einbinden der Klassen
-spl_autoload_register(function ($className){
-    //echo $className; die Klasse die gesucht wird
-    $ordner = ['Entity','Repository','Controller']; // die Ordner in den gesucht werden soll
+// Automatisches Laden von Klassen mit spl_autoload_register
+spl_autoload_register(function ($className) {
+    // Definieren der Ordner, in denen nach Klassen gesucht werden soll
+    $ordner = ['Entity', 'Repository', 'Controller'];
+
+    // Durchlaufen der definierten Ordner
     foreach ($ordner as $od) {
-        if (file_exists("src/$od/$className.php")){ // schaut ob es die Datei gibt
+        // Prüfen, ob die Datei für die gesuchte Klasse im aktuellen Ordner existiert
+        if (file_exists("src/$od/$className.php")) {
+            // Wenn ja, Datei einbinden
             include "src/$od/$className.php";
         }
     }
 });
 
+// Aufspalten der aktuellen URL in Segmente
+// Entfernt den Pfad "/verlag/" aus der URL und teilt den Rest anhand von "/"
+$url = explode('/', str_ireplace('/verlag/', '', $_SERVER['REQUEST_URI']));
 
+// Ermitteln des Controller-Namens aus dem ersten URL-Segment
+$controllerName = $url[0] . 'Controller';
 
-
-$url = explode('/',str_ireplace('/verlag/','',$_SERVER['REQUEST_URI']));
-$controllerName = $url[0].'Controller';
+// Ermitteln der Methode aus dem zweiten URL-Segment
 $method = $url[1];
+
+// Drittes URL-Segment als Parameter übergeben (z. B. eine ID), standardmäßig ein leerer Array
 $index = [(int)$url[2]] ?? [];
 
-
+// Erstellen einer Instanz des Controllers
 $controller = new $controllerName;
-call_user_func_array([$controller,$method],$index);
-//var_dump($controller);
 
-//"author/index/"
-//"author/show/1"
-//"author/delete/1"
-//"author/edit/1"
-//"author/new/"
-
-
-
-
-
-
-
+// Aufruf der Methode des Controllers mit dem gegebenen Parameter
+call_user_func_array([$controller, $method], $index);
 
 
 
--- src/Repository/AbstractRepository.php
+++ src/Repository/AbstractRepository.php
@@ -1,108 +1,138 @@
 <?php
 
+// Abstrakte Basisklasse für alle Repository-Klassen (z. B. zur Datenbankkommunikation)
 abstract class AbstractRepository
 {
+    // Name der zugehörigen Tabelle (muss von der konkreten Kindklasse gesetzt werden)
     protected string $tablename;
 
+    // Erstellt und gibt ein PDO-Datenbankobjekt zurück
     public function Dbcon(): PDO
     {
         return new PDO("mysql:host=localhost;dbname=verlag;charset=utf8mb4", 'root', '');
     }
 
+    // Führt eine SQL-Abfrage aus und gibt das Ergebnis als Array zurück
     public function query($sql, $data = []): array|false
     {
-        $dbcon = $this->Dbcon();
-        $stm = $dbcon->prepare($sql);
-        $result = $stm->execute($data);
-        $return = $stm->fetchALL(PDO::FETCH_ASSOC);
-        $id = $dbcon->lastInsertId();
+        $dbcon = $this->Dbcon();                    // Verbindung zur Datenbank herstellen
+        $stm = $dbcon->prepare($sql);               // SQL-Anweisung vorbereiten
+        $result = $stm->execute($data);             // Ausführung der Abfrage mit Platzhalterdaten
+        $return = $stm->fetchAll(PDO::FETCH_ASSOC); // Alle Zeilen als assoziatives Array holen
+        $id = $dbcon->lastInsertId();               // Prüfen, ob ein neuer Datensatz eingefügt wurde
+
         if ($id) {
+            // Wenn ja, Rückgabe der neuen ID
             $return = ['id' => (int)$id];
         }
-        if ($result) {
-            return $return;
-        } else {
-            return false;
-        }
 
+        // Rückgabe des Ergebnisses oder false bei Fehler
+        return $result ? $return : false;
     }
 
     /**
+     * Gibt alle Datensätze als Array von Entity-Objekten zurück
      * @return EntityInterface[]
      */
     public function findall(): array
     {
         $sql = "SELECT * FROM $this->tablename";
         $return = [];
-        foreach ($this->query($sql) as $item) {
-            $return[] = new $this->tablename(
-                $item);
-        }
-        return $return;
 
+        // Wandelt jede Datenbankzeile in ein Entity-Objekt um
+        foreach ($this->query($sql) as $item) {
+            $return[] = new $this->tablename($item);
+        }
+
+        return $return;
     }
 
     /**
+     * Findet einen Datensatz anhand der ID und gibt ein entsprechendes Entity-Objekt zurück
      * @param int $id
      * @return EntityInterface
      */
     public function findById(int $id): EntityInterface
     {
-
-        $sql = "SELECT * FROM $this->tablename where id = :id";
+        $sql = "SELECT * FROM $this->tablename WHERE id = :id";
         $sqldata = [':id' => $id];
+
+        // Holt das erste (und einzige) Ergebnis
         $data = $this->query($sql, $sqldata)[0];
+
+        // Gibt ein neues Entity-Objekt mit den geladenen Daten zurück
         return new $this->tablename($data);
     }
 
-
+    /**
+     * Entfernt einen Datensatz basierend auf dem Entity-Objekt
+     * @param object $obj
+     * @return bool
+     */
     public function remove(object $obj): bool
     {
-
-        $sql = "DELETE FROM $this->tablename where id = :id";
+        $sql = "DELETE FROM $this->tablename WHERE id = :id";
         $data = [':id' => $obj->getId()];
+
+        // Führt Lösch-Abfrage aus und prüft ob Ergebnis leer ist (d.h. Erfolg)
         $result = $this->query($sql, $data);
         return ($result === []);
-
     }
 
-
+    /**
+     * Aktualisiert einen vorhandenen Datensatz basierend auf einem Entity-Objekt
+     * @param EntityInterface $obj
+     * @return EntityInterface
+     */
     public function update(EntityInterface $obj): EntityInterface
     {
-        $data = $obj->mapToArray();
+        $data = $obj->mapToArray();            // Wandelt Objekt in Array um
         $keys = array_keys($data);
-        $string = ''; //':fname', ':lname' -> 'fname = :fname, lname = :lname'
+        $string = '';                          // Teil für SET-Klausel im SQL-Statement
+
+        // Erzeugt den String für das SQL-Update-Statement
         foreach ($keys as $index => $key) {
-            if ($key === ':id') {
-                continue;
-            }
+            if ($key === ':id') continue;      // ID wird im WHERE verwendet, nicht im SET
             $spalte = str_replace(':', '', $key);
             $string .= "$spalte = $key, ";
-
         }
-        $string = rtrim($string, ', ');
-        $sql = "UPDATE $this->tablename set $string where id = :id";
+
+        $string = rtrim($string, ', ');        // Entfernt das letzte Komma
+        $sql = "UPDATE $this->tablename SET $string WHERE id = :id";
+
         $this->query($sql, $data);
+
+        // Gibt das aktualisierte Objekt zurück
         return $this->findById($obj->getId());
     }
 
+    /**
+     * Erstellt einen neuen Datensatz basierend auf einem Entity-Objekt
+     * @param EntityInterface $entity
+     * @return EntityInterface
+     */
     public function create(EntityInterface $entity): EntityInterface
     {
-        $data = $entity->mapToArray();
-        unset($data[':id']);
+        $data = $entity->mapToArray();         // Objekt in Array umwandeln
+        unset($data[':id']);                   // ID wird bei neuen Einträgen nicht mitgegeben
+
         $keys = array_keys($data);
-        $spalte = '';
-        $placeholder = '';
+        $spalte = '';                          // Liste der Spalten
+        $placeholder = '';                     // Liste der Platzhalter (für prepared statement)
+
+        // Erzeugt die Listen für Spalten und Platzhalter
         foreach ($keys as $key) {
-            if ($key === ':id') {
-                continue;
-            }
+            if ($key === ':id') continue;
             $spalte .= str_replace(':', '', $key) . ', ';
             $placeholder .= "$key, ";
         }
+
         $spalte = rtrim($spalte, ', ');
         $placeholder = rtrim($placeholder, ', ');
-        $sql = "INSERT INTO $this->tablename ( $spalte ) values ( $placeholder )";
+
+        $sql = "INSERT INTO $this->tablename ( $spalte ) VALUES ( $placeholder )";
+
+        // Führt Einfüge-Abfrage aus und holt das neue Objekt per ID
         $result = $this->query($sql, $data);
         return $this->findById($result['id']);
     }
